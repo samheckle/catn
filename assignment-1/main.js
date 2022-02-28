adb32a339eafcb8a11dba437696b39062d1351f1
sam heckle <samheckle@yahoo.com>
2022-02-28 18:35:03 -0500

    added sketch 2

--- assignment-1/main.js
+++ assignment-1/main.js
@@ -1,1 +1,14 @@
-let snoowrap = require('snoowrap');
+require("dotenv").config();
+let snoowrap = require("snoowrap");
+
+const api = new snoowrap({
+  userAgent:
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:10.0) Gecko/20100101 Firefox/10.0",
+  clientId: process.env.USER_ID,
+  clientSecret: process.env.USER_SECRET,
+  refreshToken: process.env.USER_REFRESH,
+});
+
+let test = api.getSubreddit('TwoSentenceHorror')
+
+console.log(test)
--- assignment-1/readme.md
+++ assignment-1/readme.md
@@ -1,6 +1,7 @@
 ## dependencies
-https://browserify.org/#install
-https://github.com/not-an-aardvark/snoowrap
+* https://browserify.org/#install
+* https://github.com/not-an-aardvark/snoowrap
+* https://www.npmjs.com/package/dotenv
 
 ## to rebuild
 browserify main.js -o bundle.js
